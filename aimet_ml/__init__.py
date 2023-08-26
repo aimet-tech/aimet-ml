7cbf28bda2e612f1f29b8e10072dbd943be35f95
pasitj-aimet <pasit.j@aimet.tech>
2023-08-26 22:16:51 +0700

    add ffmpeg to PATH at first call

--- aimet_ml/__init__.py
+++ aimet_ml/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for aimet-ml."""
+import static_ffmpeg
+
 from . import features, processing, utils
+
+static_ffmpeg.add_paths()
 
 __author__ = """Pasit Jakkrawankul"""
 __email__ = 'pasit.j@aimet.tech'
