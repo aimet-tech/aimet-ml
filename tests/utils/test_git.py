8e801bb74ef56f31e0929704f6c6b0c45f819138
pasitj-aimet <pasit.j@aimet.tech>
2023-10-19 18:28:47 +0700

    add unit testing for aws utils

--- tests/utils/test_git.py
+++ tests/utils/test_git.py
@@ -18,9 +18,11 @@ def test_get_commit_id_short(in_git_repository):
     commit_id = get_commit_id(short=True)
     assert len(commit_id) == 7  # Short commit ID should be 7 characters
     assert commit_id.isalnum()
+    assert commit_id == os.popen("git rev-parse --short HEAD").read().strip()
 
 
 def test_get_commit_id_full(in_git_repository):
     commit_id = get_commit_id(short=False)
     assert len(commit_id) == 40  # Full commit ID should be 40 characters
     assert commit_id.isalnum()
+    assert commit_id == os.popen("git rev-parse HEAD").read().strip()
