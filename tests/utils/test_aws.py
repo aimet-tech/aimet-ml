881b2b24436d48ffb820e220eb11e7d8ab925925
aimet-pasitpk <pasit.j@aimet.tech>
2023-10-27 00:02:13 +0700

    add docstrings & type hints

--- tests/utils/test_aws.py
+++ tests/utils/test_aws.py
@@ -1,12 +1,13 @@
 import os
 from datetime import datetime
+from pathlib import Path
 
 import pytest
 from dotenv import find_dotenv, load_dotenv
 
 from aimet_ml.utils.aws import download_s3, upload_dir_s3, upload_files_s3
 
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(), override=True)
 
 AWS_S3_BUCKET = os.environ.get("AWS_S3_BUCKET")
 CURRENT_TIME = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -16,18 +17,44 @@ if AWS_S3_BUCKET is None:
 
 
 @pytest.fixture(scope="module")
-def tmp_dir(tmp_path_factory):
+def tmp_dir(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    """
+    Fixture for creating a temporary directory using pytest's TempPathFactory.
+
+    Args:
+        tmp_path_factory (pytest.TempPathFactory): Pytest fixture for creating temporary directories.
+
+    Returns:
+        pathlib.Path: The temporary directory path.
+    """
     tmp_dir = tmp_path_factory.mktemp("temp_dir")
     yield tmp_dir
 
 
-def download_and_check(file_path, expected_content, tmp_dir):
+def download_and_check(file_path: str, expected_content: str, tmp_dir: Path):
+    """
+    Downloads a file from AWS S3 and checks its content against the expected content.
+
+    Args:
+        file_path (str): The file path on AWS S3.
+        expected_content (str): The expected content of the file.
+        tmp_dir (pathlib.Path): The local temporary directory where the file will be downloaded.
+
+    Raises:
+        AssertionError: If the downloaded file content does not match the expected content.
+    """
     download_s3(AWS_S3_BUCKET, file_path, os.path.join(tmp_dir, os.path.basename(file_path)))
     with open(os.path.join(tmp_dir, os.path.basename(file_path)), "r") as file:
         assert file.read() == expected_content
 
 
-def test_download_s3(tmp_dir):
+def test_download_s3(tmp_dir: Path):
+    """
+    Test function for downloading files from AWS S3 and checking their content.
+
+    Args:
+        tmp_dir (pathlib.Path): The local temporary directory for storing downloaded files.
+    """
     test_files = {
         "test_download_s3/test_file_1.txt": "hello, test file 1",
         "test_download_s3/test_file_2.txt": "hello, test file 2",
@@ -37,7 +64,13 @@ def test_download_s3(tmp_dir):
         download_and_check(file_path, content, tmp_dir)
 
 
-def test_upload_files_s3(tmp_dir):
+def test_upload_files_s3(tmp_dir: Path):
+    """
+    Test function for uploading files to AWS S3 and then downloading and checking their content.
+
+    Args:
+        tmp_dir (pathlib.Path): The local temporary directory for storing uploaded and downloaded files.
+    """
     unique_dir_path = f"test_upload_files/{CURRENT_TIME}"
     uploaded_files = [tmp_dir / f"test_file_{i}.txt" for i in range(1, 4)]
 
@@ -47,7 +80,13 @@ def test_upload_files_s3(tmp_dir):
         download_and_check(f"{unique_dir_path}/test_file_{i}.txt", f"hello, test file {i}", tmp_dir)
 
 
-def test_upload_dir_s3(tmp_dir):
+def test_upload_dir_s3(tmp_dir: Path):
+    """
+    Test function for uploading a directory to AWS S3 and then downloading and checking the content.
+
+    Args:
+        tmp_dir (pathlib.Path): The local temporary directory for storing uploaded and downloaded files.
+    """
     unique_dir_path = f"test_upload_dir/{CURRENT_TIME}"
 
     upload_dir_s3(AWS_S3_BUCKET, unique_dir_path, tmp_dir)
--- tests/utils/test_git.py
+++ tests/utils/test_git.py
@@ -7,7 +7,11 @@ from aimet_ml.utils.git import get_commit_id
 
 @pytest.fixture
 def in_git_repository():
-    # Check if we are in a git repository
+    """
+    Fixture to check if the code is running inside a Git repository.
+
+    If not in a Git repository, it will skip the test.
+    """
     in_repo = os.system("git rev-parse --is-inside-work-tree") == 0
     if not in_repo:
         pytest.skip("Not in a git repository.")
@@ -15,6 +19,7 @@ def in_git_repository():
 
 
 def test_get_commit_id_short(in_git_repository):
+    """Test the get_commit_id function with short=True option."""
     commit_id = get_commit_id(short=True)
     assert len(commit_id) == 7  # Short commit ID should be 7 characters
     assert commit_id.isalnum()
@@ -22,6 +27,7 @@ def test_get_commit_id_short(in_git_repository):
 
 
 def test_get_commit_id_full(in_git_repository):
+    """Test the get_commit_id function with short=False option."""
     commit_id = get_commit_id(short=False)
     assert len(commit_id) == 40  # Full commit ID should be 40 characters
     assert commit_id.isalnum()
