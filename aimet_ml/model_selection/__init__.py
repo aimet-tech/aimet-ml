a2f57aa27300e0640d010a316ac5a45a7b6c4174
aimet-pasitpk <pasit.j@aimet.tech>
2024-03-06 16:49:31 +0700

    Add new split method

--- aimet_ml/model_selection/__init__.py
+++ aimet_ml/model_selection/__init__.py
@@ -1,1 +1,1 @@
-from .splits import get_splitter, join_cols, split_dataset, stratified_group_split
+from .splits import get_splitter, join_cols, split_dataset, split_dataset_v2, stratified_group_split
